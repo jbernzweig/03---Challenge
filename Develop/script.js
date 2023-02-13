652fb64e1656c2560923d710be8c107b784f31c6
Jason Bernzweig <jasonbernzweig@jasons-air.lan>
2023-02-13 11:25:45 -0500

    updates to include all letters A-Z

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,8 @@
 // // Assignment Code
 // <button id="generate" class="btn">Generate Password</button>
 var generateBtn = document.querySelector("#generate"); //Targets the generate button
-var upperCase = ["A", "B", "C", "D", "E", "F"]
-var lowerCase = ["a", "b", "c", "d", "e", "f"]
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(",]
 // Need alerts for 
